1b93f22a2ec36ca9f758878f88f0fcdb66ff8f03
rohmanagus <52437354+rohmanagus@users.noreply.github.com>
2019-08-26 15:56:34 +0700

    Add files via upload

---
+++ liveCode/liveCode-2.js
@@ -0,0 +1,10 @@
+var name = 'arief'; // Isi nama disini
+ 
+// Code disini
+if(name[0] === 'a' || name[0] === 'b' || name[0] === 'c' || name[0] === 'd'){
+  console.log('Masuk Group Pertama')
+}else if(name[0] === 'e' || name[0] === 'f' || name[0] === 'g' || name[0] === 'h'){
+  console.log('Masuk grop kedua')
+}else{
+  console.log('group terakhir')
+}
